055330518ecd375b0569417e76acac3d9f26acf6
Rohan Panwar <rohan.panwar@neurontechlabs.com>
2020-07-14 21:48:46 +0530

    added redux functionality and secretWord reducer

--- src/reducers/guessedWordsReducer.js
+++ src/reducers/guessedWordsReducer.js
@@ -1,10 +1,10 @@
-import { actionTypes } from '../actions';
+import { actionTypes } from "../actions";
 
-export default (state=[], action) => {
-    switch(action.type) {
-        case actionTypes.GUESS_WORD:
-            return [...state, action.payload]
-        default:
-            return state;
-    }
-} 
+export default (state = [], action) => {
+  switch (action.type) {
+    case actionTypes.GUESS_WORD:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
